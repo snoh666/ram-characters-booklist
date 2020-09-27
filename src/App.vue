e4d6cfe174af3708a40f3c9988339a128288cccf
Snochowski Mikołaj <snochopl@gmail.com>
2020-09-27 20:42:24 +0200

    Import new Planets component

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,17 @@
   <h1>
     Hello Vue application
   </h1>
+  <Planets />
 </template>
 
 <script>
+import Planets from './components/Planets';
+
 export default {
   name: 'App',
+  components: {
+    Planets,
+  }
 }
 </script>
 
