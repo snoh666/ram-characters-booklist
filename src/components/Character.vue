0f65ea38385532d16abf6fbced6511dbb7c8bc22
Snochowski Mikołaj <snochopl@gmail.com>
2020-09-28 00:14:13 +0200

    Add Character component

---
+++ src/components/Character.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="character__wrapper">
+    <img v-bind:src="imageUrl" alt="Character Face">
+    <h6>{{name}}</h6>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Character",
+  props: {
+    name: String,
+    imageUrl: String,
+    status: Boolean,
+  },
+}
+</script>
+
+<style scoped>
+  .character__wrapper {
+    margin: 1rem 0;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  img {
+    width: 64px;
+    height: 64px;
+  }
+</style>
