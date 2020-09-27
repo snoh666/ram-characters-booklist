06c6578b8e8966459505a5d7a4859e0f52547498
Snochowski Mikołaj <snochopl@gmail.com>
2020-09-27 20:51:18 +0200

    Add child single Planet component

---
+++ src/components/Planet.vue
@@ -0,0 +1,18 @@
+<template>
+  <h5>
+    {{name}}
+  </h5>
+</template>
+
+<script>
+export default {
+  name: "Planet",
+  props: {
+    name: String,
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
